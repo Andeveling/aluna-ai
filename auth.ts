ca3f7204caccdb1f9096499799c771ccd018ea7f
Fabian Andres Parra Sanchez <andeveling@gmail.com>
2025-09-06 08:16:59 -0500

    fix(auth): remove Prisma adapter from NextAuth configuration

--- auth.ts
+++ auth.ts
@@ -1,9 +1,6 @@
 import NextAuth from 'next-auth';
 import google from 'next-auth/providers/google';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import prisma from '@/lib/prisma-client';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
   providers: [google],
 });
